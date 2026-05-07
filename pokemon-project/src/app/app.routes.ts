1ba59b4d528a85cfd80f4ca6a5ae0d9ce959bc73
FrancoD21 <d.francosanchez@campus.unimib.it>
2026-05-07 12:36:28 +0000

    Creazione dei componenti e impostazione delle routes

---
+++ pokemon-project/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { ListaTipi } from './lista-tipi/lista-tipi';
+import { ListaPokemon } from './lista-pokemon/lista-pokemon';
+import { DettagliPokemon } from './dettagli-pokemon/dettagli-pokemon';
+
+
+export const routes: Routes = [
+  { path: 'tipi', component: ListaTipi },
+  { path: 'lista/:type', component: ListaPokemon },
+  { path: 'dettagli/:id', component: DettagliPokemon },
+  { path: '', redirectTo: '/tipi', pathMatch: 'full' } // Redirezione automatica [cite: 282]
+];
